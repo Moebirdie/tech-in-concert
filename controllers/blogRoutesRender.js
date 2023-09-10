b4f2337bc8730770fffb162d63b154cbe76296e9
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-09-10 11:23:10 -0400

    picking back up again to finish. Added comment functionality and organized open items into issues

---
+++ controllers/blogRoutesRender.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const Blog  = require('../models/Blog');
+
+
+//get all blogs
+router.get('/blog', async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+        include: [{ model: Comment }, {model: User}],
+      });
+    res.status(200).json(blogData);  
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const newBlog = await Blog.findByPk(req.params.id,{
+       title: req.body.title,
+       summaryText: req.body.summaryText,
+       bodyText: req.body.bodyText,
+       //user_id: req.body.user_id,
+       comment_id: req.body.comment_id,
+       user_id: req.session.user_id
+  });
+    res.status(200).json(newBlog);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
